39728d1020764a11907cbd6fdfa5356bd20193a7
Garret Reichenbach <garret2kool@gmail.com>
2023-11-29 11:47:42 -0700

    finally got it to package correctly

---
+++ package.js
@@ -0,0 +1,28 @@
+const packager = require('electron-packager');
+const path = require('path');
+const fs = require('fs');
+
+const options = {
+    dir: '.',
+    out: './release-builds',
+    name: 'StarMade Launcher',
+    overwrite: true,
+    icon: path.join(__dirname, 'assets', 'starmade'),
+    asar: true,
+}
+
+const platforms = [
+    { platform: 'darwin', arch: 'x64', icon: options.icon + '.icns' },
+    { platform: 'win32', arch: 'ia32', icon: options.icon + '.ico' },
+    { platform: 'linux', arch: 'x64', icon: options.icon + '.png' }
+]
+
+for (let opts of platforms) {
+    packager(Object.assign({}, options, opts), function (error, appPaths) {
+        if (error) {
+            console.error(`Error packaging app for platform ${opts.platform}: ${error}`);
+        } else {
+            console.log(`Packaged to ${appPaths}`);
+        }
+    });
+}
--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,5 +1,3 @@
-/**
- * All we have to do is run the StarMade launcher jar. The only reason we are using js is for the custom icon.
- */
 var spawn = require('child_process').spawn;
-var child = spawn('java', ['-jar', 'starmade-launcher.jar']);
+spawn('java', ['-jar', 'starmade-launcher.jar']);
+process.exit(0);
