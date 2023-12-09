df3eeb9adbac4c11f838f8f07cded8c9825d49b7
Garret Reichenbach <garret2kool@gmail.com>
2023-12-08 20:54:36 -0700

    bump version

--- src/main/java/smlauncher/StarMadeLauncher.java
+++ src/main/java/smlauncher/StarMadeLauncher.java
@@ -43,7 +43,7 @@ import java.util.zip.ZipFile;
 public class StarMadeLauncher extends JFrame {
 
 	public static final String BUG_REPORT_URL = "https://github.com/garretreichenbach/New-StarMade-Launcher/issues";
-	public static final String LAUNCHER_VERSION = "3.0.4"; //We've had two other launchers before this
+	public static final String LAUNCHER_VERSION = "3.0.5"; //We've had two other launchers before this
 	private static final String[] J18ARGS = {
 			"--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED",
 			"--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
