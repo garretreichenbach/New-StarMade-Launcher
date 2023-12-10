3a972aca94823feb1382122d87c22ae1bfe7ac99
SlavSquatSuperstar <ericweng03@gmail.com>
2023-12-09 22:35:48 -0500

    Add JUnit 5 test dependency

---
+++ src/test/java/smlauncher/ExampleTest.java
@@ -0,0 +1,14 @@
+package smlauncher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+
+	@Test
+	void exampleTestPass() {
+		assertTrue(true);
+	}
+
+}
