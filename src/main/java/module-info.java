40f51e66b1c7a79a63497dcfdb7f3f40d0c7f86f
Garret Reichenbach <garret2kool@gmail.com>
2024-08-29 15:10:08 -0700

    Update Windows binary artifact name in Github Workflow
    This commit changes the name of the Windows binary in the package workflow, the binary was named "StarMade-Launcher-Windows", now it has been appropriately renamed to "StarMade-Launcher.exe". This change provides a more accurate and standard naming convention for Windows executable files.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module Launcher {
+	requires com.formdev.flatlaf;
+	requires it.unimi.dsi.fastutil;
+	requires jasypt;
+	requires java.datatransfer;
+	requires java.desktop;
+	requires java.management;
+	requires java.xml;
+	requires org.apache.commons.io;
+	requires org.json;
+	requires plexus.archiver;
+}
