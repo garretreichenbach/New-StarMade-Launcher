3ecae5f69220e1f9b94df8996f2fe5195df3c259
Garret Reichenbach <garretreichenbach@gmail.com>
2024-08-29 21:04:26 -0700

    almost got working, but it crashes my pc lmao

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
-module Launcher {
-	requires com.formdev.flatlaf;
-	requires it.unimi.dsi.fastutil;
-	requires jasypt;
+module smlauncher {
 	requires java.datatransfer;
 	requires java.desktop;
 	requires java.management;
 	requires java.xml;
-	requires org.apache.commons.io;
 	requires org.json;
+	requires org.apache.commons.io;
 	requires plexus.archiver;
+	requires com.formdev.flatlaf;
+	requires jdk.management;
+	requires jasypt;
 }
